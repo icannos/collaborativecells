910ed8d81079cb4caf6e3ed2c18c2847d338fd03
Maxime <maxime.darrin@ens-lyon.fr>
2019-05-27 14:51:03 +0200

    template for MaddpgAgent

--- src/maddpgAgent.py
+++ src/maddpgAgent.py
@@ -1,4 +1,10 @@
 from collections import deque
+from keras import Model
+
+from keras import backend as K
+from tensorflow.python.ops.parallel_for import jacobian
+import tensorflow as tf
+
 import random
 
 
@@ -8,9 +14,18 @@ class AbstractMaddpgAgent:
         self.observation_dim = observation_dim
         self.action_dim = action_dim
 
+        # Should be something, one day, maybe it will be something else, dont know yet
         # Should be keras/tensorflow models (Support predict, train)
         self.critic = None
         self.policy = None
+
+
+        # Private members
+        self._state_input = None
+        self._action_input = None
+
+        self._critic_gradient = None
+        self._policy_jacobian = None
 
     def mk_critic_model(self):
         raise NotImplemented
@@ -18,8 +33,22 @@ class AbstractMaddpgAgent:
     def mk_policy_model(self):
         raise NotImplemented
 
-    def act(self, state, exploration=True):
-        action = self.policy.predict(state)[0]
+    def mk_critic_action_gradient(self):
+        self._critic_gradient = K.gradients(self.critic.output, self._action_input)
+
+    def mk_policy_jacobian(self):
+        self._policy_jacobian = jacobian(self.policy.output, self.policy.trainable_weights)
+
+    def critic_action_gradient(self, s, a):
+        s = K.get_session()
+        return s.run([self._critic_gradient], feed_dict={self._state_input: s, self._action_input: a})[0]
+
+    def policy_jacobian(self):
+        s = K.get_session()
+        return s.run([self._policy_jacobian], feed_dict={self._state_input})[0]
+
+    def act(self, observation, exploration=True):
+        action = self.policy.predict(observation)[0]
 
         return action + self.random_distrib() if exploration else action
 
@@ -31,27 +60,25 @@ class AbstractMaddpgAgent:
 
 
 class ReplayBuffer:
-    def __init__(self, nb_agent, memory_size, batch_size):
+    def __init__(self, memory_size, batch_size):
         self.batch_size = batch_size
-        self.nb_agent = nb_agent
         self.memory_size = memory_size
 
         # We store history for each agent
         # Because their rewards/trajectory are not the same
-        self.memory = [deque([], maxlen=memory_size) for _ in range(nb_agent)]
+        self.memory = deque([], maxlen=memory_size)
 
-    def sample(self, agent=None):
-        if agent is not None:
-            return random.sample(self.memory[agent], self.batch_size)
-        else:
-            return [random.sample(self.memory[agent], self.batch_size) for agent in range(self.nb_agent)]
+    def sample(self):
+        return random.sample(self.memory, self.batch_size)
 
     def remember(self, state, action, reward, next_state):
         self.memory.append((state, action, reward, next_state))
 
 
 class AbstractMaddpgTrainer:
-    def __init__(self, env, nb_agent=3, agent_class = None, memory_size=1000, batch_size=32):
+    def __init__(self, env, nb_agent=3, agent_class = None, memory_size=1000, batch_size=32, horizon=None):
+        self.horizon = horizon
+        self.env = env
         self.agent_class = agent_class
         self.nb_agent = nb_agent
         self.memory_size = memory_size
@@ -59,11 +86,26 @@ class AbstractMaddpgTrainer:
         self.action_dim = env.action_space
         self.observation_dim = env.observation_space
 
-        self.buffer = ReplayBuffer(nb_agent=nb_agent, memory_size=memory_size, batch_size=batch_size)
+        self.buffer = ReplayBuffer(memory_size=memory_size, batch_size=batch_size)
 
         self.agents = []
         for agent in range(nb_agent):
             self.agents.append(agent_class[agent](self.action_dim, self.observation_dim))
 
-    def train(self):
-        pass
+    def train(self, episode=1):
+
+        for _ in range(episode):
+            state = self.env.reset()
+
+            while True:
+                actions = [self.agents[i].act(state[i]) for i in range(self.nb_agent)]
+
+                next_state, rewards, done, info = self.env.step(actions)
+
+                self.buffer.remember(state, actions, rewards, next_state)
+
+
+
+
+
+
