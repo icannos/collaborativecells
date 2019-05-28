e1ac9373da56c54180e703a1c422989d98b54902
Maxime <maxime.darrin@ens-lyon.fr>
2019-05-28 11:24:12 +0200

    some dirty work, tests and weird tensorflow things

--- src/maddpgAgent.py
+++ src/maddpgAgent.py
@@ -4,28 +4,35 @@ from keras import Model
 from keras import backend as K
 from tensorflow.python.ops.parallel_for import jacobian
 import tensorflow as tf
+import numpy as np
 
 import random
 
 
 class AbstractMaddpgAgent:
 
-    def __init__(self, action_dim, observation_dim):
+    def __init__(self, agent_id, action_dim, observation_dim, learning_rate=0.01):
+        self.agent_id = agent_id
+        self.learning_rate = learning_rate
         self.observation_dim = observation_dim
         self.action_dim = action_dim
 
-        # Should be something, one day, maybe it will be something else, dont know yet
         # Should be keras/tensorflow models (Support predict, train)
+        # Should be something, one day, maybe it will be something else, dont know yet
+        # Maybe it should be a something in between
         self.critic = None
         self.policy = None
 
 
         # Private members
         self._state_input = None
+        self._other_action_input = None
         self._action_input = None
 
         self._critic_gradient = None
         self._policy_jacobian = None
+
+        self.policy_opti_ops = None
 
     def mk_critic_model(self):
         raise NotImplemented
@@ -38,6 +45,19 @@ class AbstractMaddpgAgent:
 
     def mk_policy_jacobian(self):
         self._policy_jacobian = jacobian(self.policy.output, self.policy.trainable_weights)
+
+    def mk_policy_opt(self):
+        self.mk_critic_action_gradient()
+        self.mk_policy_jacobian()
+
+        gradJ = tf.reduce_mean(self._policy_jacobian * self._critic_gradient)
+
+        update_ops = []
+        for i,w in enumerate(self.policy.trainable_weights):
+            update_ops.append(w + self.learning_rate * gradJ[i])
+
+        self.policy_opti_ops = update_ops
+
 
     def critic_action_gradient(self, s, a):
         s = K.get_session()
@@ -52,8 +72,17 @@ class AbstractMaddpgAgent:
 
         return action + self.random_distrib() if exploration else action
 
-    def evaluate(self, global_state):
-        return self.critic.predict(global_state)[0]
+    def Q(self, state, actions):
+        raise NotImplemented
+
+    def watch(self, state, i):
+        """
+        Take a state and return an observation, ie what this agent actually sees. By default, just returns the ith
+        element of state
+        :param state: state of the get from the environnment
+        :return: observation, what THIS agent sees
+        """
+        raise NotImplemented
 
     def random_distrib(self):
         raise NotImplemented
@@ -76,7 +105,8 @@ class ReplayBuffer:
 
 
 class AbstractMaddpgTrainer:
-    def __init__(self, env, nb_agent=3, agent_class = None, memory_size=1000, batch_size=32, horizon=None):
+    def __init__(self, env, nb_agent=3, agent_class = None, memory_size=1000, batch_size=32, gamma=0.95, horizon=None):
+        self.gamma = gamma
         self.horizon = horizon
         self.env = env
         self.agent_class = agent_class
@@ -103,6 +133,37 @@ class AbstractMaddpgTrainer:
                 next_state, rewards, done, info = self.env.step(actions)
 
                 self.buffer.remember(state, actions, rewards, next_state)
+
+                state = next_state
+
+
+    def update_critic(self, sample):
+
+        y = [[] for i in range(self.nb_agent)]
+        X = [[] for i in range(self.nb_agent)]
+
+        for state, actions, rewards, next_state in sample:
+
+            # First we build the target value y:
+            actions = []
+
+            for k in range(self.nb_agent):
+                agent = self.agents[k]
+                observation = agent.watch(state, k)
+                action = agent.act(observation)
+
+                actions.append(action)
+
+            for i in range(self.nb_agent):
+                y[i].append(rewards[i] + self.gamma * self.agents[i].Q(state, actions))
+                X[i].append(np.asarray([state, actions]))
+
+        for i in range(self.nb_agent):
+            self.agents[i].critic.train_on_batch(X[i], y[i])
+
+
+
+
 
 
 
---
+++ src/test.py
@@ -0,0 +1,52 @@
+import keras.backend as K
+from keras.models import Sequential, Model
+from keras.layers import Dense, Activation, InputLayer, Add, Input
+from tensorflow.python.ops.parallel_for import jacobian
+import tensorflow as tf
+import numpy as np
+
+model = Sequential()
+model.add(InputLayer((100,), name="in_1"))
+model.add(Dense(32))
+model.add(Activation('relu'))
+
+
+
+model2 = Sequential()
+model2.add(InputLayer((100,), name="in_2"))
+model2.add(Dense(32))
+model2.add(Activation('relu'))
+
+i1 = Input((100,), name="in1")
+i2 = Input((100,), name="in2")
+output = Add()([model(i1), model2(i2)])
+
+M = Model(inputs=[i1, i2], outputs=output)
+
+M.compile(optimizer="adam", loss="mse")
+
+
+i1 = tf.placeholder("float32", shape=(None,100))
+i2 = tf.placeholder("float32", shape=(None, 100))
+
+tensor = M([i1,i2])
+
+j = jacobian(tensor, M.trainable_weights)
+
+print(type(j))
+print(len(j))
+
+X1 = np.random.uniform(-5, 5, (10, 100))
+X2 = np.random.uniform(-5, 5, (10, 100))
+Y = np.random.uniform(-5, 5, (10, 100))
+
+a = tf.Variable(initial_value=0)
+b = tf.Variable(initial_value=1)
+c = tf.Variable(initial_value=3)
+
+
+
+with K.get_session() as s:
+    j = s.run([j], feed_dict={i1: X1, i2:X2})[0]
+
+
