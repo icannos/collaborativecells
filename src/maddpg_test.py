74b30f3aed6367b9a07588f2ee600434a27c9a00
Maxime <maxime.darrin@ens-lyon.fr>
2019-06-19 09:04:50 +0200

    convergence of single agent with maddpg

--- src/maddpg_test.py
+++ src/maddpg_test.py
@@ -7,7 +7,7 @@ import numpy as np
 import tensorflow as tf
 
 from multiagent.environment import MultiAgentEnv
-from maddpgAgent import DenseAgent, AbstractMaddpgTrainer
+from maddpgAgentv2 import DenseAgent, AbstractMaddpgTrainer
 
 
 import multiagent.scenarios as scenarios
@@ -30,6 +30,6 @@ env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.obser
 
 session = tf.Session()
 
-trainer = AbstractMaddpgTrainer(session, env, 1, [DenseAgent for _ in range(1)])
+trainer = AbstractMaddpgTrainer(session, "trainer", env, 3, [DenseAgent for _ in range(3)])
 
 trainer.train(10000, render=True)
